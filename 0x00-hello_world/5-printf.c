abce641904b34aef936770fe99f26c6cb13ad7ae
valerc123 <valentina.rua123@gmail.com>
2019-06-19 21:48:01 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-[2;2R[2;2R
+
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("%s\n", "with proper grammar, but the outcome is a piece of art");
+return (0);
+}
