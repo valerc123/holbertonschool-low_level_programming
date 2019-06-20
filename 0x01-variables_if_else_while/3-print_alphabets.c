ad921ddaf9b3e18a4b8bcd21379a5a5ee6ac0d07
valerc123 <valentina.rua123@gmail.com>
2019-06-20 20:06:57 +0000

    Putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,6 +9,10 @@
 int main(void)
 {
 char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
 for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 {
 putchar(alphabet);
