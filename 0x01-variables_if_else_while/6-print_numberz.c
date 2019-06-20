f2452f2972013d989756dcfb326f4961f6820d5d
valerc123 <valentina.rua123@gmail.com>
2019-06-20 21:07:32 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
