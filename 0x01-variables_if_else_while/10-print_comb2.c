7d9c64416cc42901feb49a87a37d0951b709dab2
valerc123 <valentina.rua123@gmail.com>
2019-06-20 23:04:17 +0000

    print_comb2

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n, j;
+for (n = 48; n < 58; n++)
+{
+for( j = 48; j < 58; j++)
+{
+putchar(n);
+putchar(j);
+if (n == 57 && j == 57) 
+{
+continue;
+}
+else
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+putchar('\n');
+return (0);
+}
