49091db479d455e9dd18f09c0dcca43a2e2e99ff
valerc123 <valentina.rua123@gmail.com>
2019-06-25 17:34:07 +0000

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _isalpha - checks whether the passed character is an alphabet (A-Z or a-z)
+ * @c: The character to check
+ *
+ * Return: 1 if c is an alphabet, otherwise Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
