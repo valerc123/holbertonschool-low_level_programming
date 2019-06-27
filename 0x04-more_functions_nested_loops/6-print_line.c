3a841e2e8bea0ce37ab397176083c88f19ad84a6
valenrc123 <valentina.rua123@gmail.com>
2019-06-27 20:15:15 +0000

    print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,17 @@
 #include "holberton.h"
 
 /**
- * print_line - This function draws a straight line in the terminal.
- * @c: The character to check
- * Return: 1 if c is lowercase, otherwise Return: 0
+ * print_line - This function draws a straight line n times.
+ * @n:  is the number of times the character _ should be printed
+ * Return: void
  */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
