90d5de1c77087652efc453545f8f7ff3edd57475
valenrc123 <valentina.rua123@gmail.com>
2019-06-27 15:39:02 +0000

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _isupper - This function checks whetever uppercase character.
+ * @c: character to checked
+ *
+ * Return: the sum of the two operands
+ */
+
+int _isupper(int c)
+{
+	if (c >= 41 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
