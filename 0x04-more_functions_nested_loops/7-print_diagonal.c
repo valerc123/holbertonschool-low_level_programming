9d8a4000b75c8492a53bbe2094d14e6c208e06b8
valenrc123 <valentina.rua123@gmail.com>
2019-06-27 21:20:06 +0000

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * print_diagonal - This function draws a diagonal line
+ * @n: Number of times the character \ should be printed
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, a;
+
+	if(n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{	
+
+			if (i > 0)
+			{
+				for (a = 0; a < i; a++)
+				{
+					_putchar(32);
+				}
+				_putchar(92);	
+				_putchar('\n');
+			}
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
