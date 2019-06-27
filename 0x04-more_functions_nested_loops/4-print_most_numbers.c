08589a26c84a876119d734f091d9008be62f3a5d
valenrc123 <valentina.rua123@gmail.com>
2019-06-27 16:57:33 +0000

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - This function prints the numbers from 0 to 9
+ * Description: Don't print 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+	}
+}
