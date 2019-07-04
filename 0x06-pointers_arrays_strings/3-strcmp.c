b5fad3f66cd37328a635d5f1796f0865144cb3ce
valenrc123 <valentina.rua123@gmail.com>
2019-07-04 22:00:28 +0000

    compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,6 +5,8 @@
  * @s1: This is the first string to be compared
  * @s2: This is the second string to be compared
  * Return: if Return value < 0 then it indicates str1 is less than str2.
+ * if Return value > 0 then it indicates str2 is less than str1.
+ * if Return value = 0 then it indicates str1 is equal to str2.
 */
 
 int _strcmp(char *s1, char *s2)
