b2eaaf16ac32138ffd9dac534e623a2ff0d4ed72
valerc123 <valentina.rua123@gmail.com>
2019-07-05 02:44:29 +0000

    lowercase letters

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,11 +10,11 @@ char *string_toupper(char *n)
 {
 	int i;
 
-	for(i = 0; n[i] != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
 		if (n[i] >= 97 && n[i] <= 122)
 		{
-			n[i] -= 32; 
+			n[i] -= 32;
 		}
 	}
 	return (n);
