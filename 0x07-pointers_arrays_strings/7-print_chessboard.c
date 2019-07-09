d459611aa7efb66bbb9d5b23f6e46a502028b824
valerc123 <valentina.rua123@gmail.com>
2019-07-09 04:19:44 +0000

    chessboar

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - print chessboard from array of pieces and locations
+ * @a: 2D array of pieces and locations
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; ++i)
+	{
+		for (j = 0; j < 8; ++j)
+		{	
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
