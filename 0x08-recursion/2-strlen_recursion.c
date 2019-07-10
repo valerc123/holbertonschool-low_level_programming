79b45340fbe250ee94585b441af3241549702b9f
valenrc123 <valentina.rua123@gmail.com>
2019-07-10 18:58:20 +0000

    length string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- * _strlen_recursion - This function returns the length of a string 
+ * _strlen_recursion - This function returns the length of a string
  * @s: string for count
- * Return: length of string 
+ * Return: length of string
  */
 
 int _strlen_recursion(char *s)
@@ -14,6 +14,6 @@ int _strlen_recursion(char *s)
 	}
 	else
 	{
-		return(1 + _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
