09bc759536e4da766535b366e3b35794afd9e57e
valerc123 <valentina.rua123@gmail.com>
2019-07-12 01:46:01 +0000

    is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,35 @@
 #include "holberton.h"
 
 /**
- * is_prime_number -  
+ * is_prime_number - Function that return if the input is prime number
+ * @n: Number to be valued
+ * Return: Return 1 if is prime or 0 otherwise
  */
-
-int is_prime_number(int n)
+int is_prime_number(int x)
 {
-	if ((n % 1 == 0) && (n % n == 0))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+   static int  cont = 2;
+   static int  result = 1;
+
+    if (x == 1 || x == 0)
+    {
+        cont = 2;
+        result = 0;
+    }
+
+    if (cont < x)
+    {
+        if ((x % cont) == 0)
+        {
+            cont = 2;
+            result = 0;
+        }
+        else
+        {
+            result = 1;
+            cont++;
+            is_prime_number(x);
+        }
+    } 
+    cont = 2;
+    return result;
 }
