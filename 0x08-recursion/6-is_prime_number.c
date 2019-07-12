1771199c03b4954f9d124dcde4f97130685587b6
valerc123 <valentina.rua123@gmail.com>
2019-07-12 01:54:21 +0000

    is prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,34 +2,33 @@
 
 /**
  * is_prime_number - Function that return if the input is prime number
- * @n: Number to be valued
+ * @x: Number to be valued
  * Return: Return 1 if is prime or 0 otherwise
  */
 int is_prime_number(int x)
 {
-   static int  cont = 2;
-   static int  result = 1;
+	static int  cont = 2;
+	static int  result = 1;
 
-    if (x == 1 || x == 0)
-    {
-        cont = 2;
-        result = 0;
-    }
-
-    if (cont < x)
-    {
-        if ((x % cont) == 0)
-        {
-            cont = 2;
-            result = 0;
-        }
-        else
-        {
-            result = 1;
-            cont++;
-            is_prime_number(x);
-        }
-    } 
-    cont = 2;
-    return result;
+	if (x == 1 || x == 0)
+	{
+		cont = 2;
+		result = 0;
+	}
+	if (cont < x)
+	{
+		if ((x % cont) == 0)
+		{
+			cont = 2;
+			result = 0;
+		}
+		else
+		{
+			result = 1;
+			cont++;
+			is_prime_number(x);
+		}
+	}
+	cont = 2;
+	return (result);
 }
