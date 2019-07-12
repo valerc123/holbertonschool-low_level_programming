e208a78fe396f5136df6c4827c684c5a2edec94e
valerc123 <valentina.rua123@gmail.com>
2019-07-12 02:25:15 +0000

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,38 @@
 #include "holberton.h"
-
 /**
-  *
-  *
+ * root - fint the square root of a number
+ * @n: integer to set if is the square root
+ * @y: integer value to take square root of
+ * Return: square root of n
+ */
+
+int root_square(int n,int y)
+{
+        if ((y * y) > n)
+        {
+                return (-1);
+        }
+        else if (y * y == n)
+        {
+                return (y);
+        }
+        else
+        {
+                return (root_square(n, y + 1));
+        }
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find squre root
+ * Return: square root of n
  */
 int _sqrt_recursion(int n)
 {
+	int y = 0;
 	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 1)
-	{
-		return (-1);
-	}
-	else if (n == 0)
-	{
-		return root_square(n, 1);
-	}
+        {
+                return (-1);
+        }
+		n = root_square(n, y + 1);
+		return (n);
 }
-
-int root_square(n, y)
-{
-	if(y * y == n)
-	{
-		return (y);
-	}
-	
-}
-
