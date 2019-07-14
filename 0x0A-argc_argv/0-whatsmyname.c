d22121096543a612183322a6c7c74532e4951548
valerc123 <valentina.rua123@gmail.com>
2019-07-14 05:06:24 +0000

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- prints its name
+ * @argc: number of things in the command line
+ * @argv: array of string values
+ * Return: 0
+*/
+int main (int argc, char* argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}	
