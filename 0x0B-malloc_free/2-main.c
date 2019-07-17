953374288776ba39c29d5d11b3d24bc155144edb
valerc123 <valentina.rua123@gmail.com>
2019-07-17 01:57:38 +0000

    concatenates two strings

---
+++ 0x0B-malloc_free/2-main.c
@@ -0,0 +1,20 @@
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *s;
+
+	s = str_concat("Betty ", "Holberton");
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
+}
