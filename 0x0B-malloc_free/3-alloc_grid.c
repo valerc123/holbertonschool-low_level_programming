fb31190e4c083489d8dd5fe56134814cf33f1a60
valerc123 <valentina.rua123@gmail.com>
2019-07-17 16:29:57 +0000

    retur pointer 2 dimensional array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function that returns a pointer
+ * @width: width
+ * @height: height
+ * Return: pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arg;
+	int i, j;
+
+	arg = malloc(sizeof(int *) * height);
+	if (arg == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		arg[i] = malloc(sizeof(int) * width);
+		if (arg[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				i--;
+				free(arg[i]);
+			}
+		free(arg);
+		return (NULL);
+		}
+		for (j = 0; j <= i; j++)
+		{
+			arg[i][j] = 0;
+		}
+	}
+	return (arg);
+}
