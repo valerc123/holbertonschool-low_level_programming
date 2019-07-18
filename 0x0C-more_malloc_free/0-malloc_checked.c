d21f2251bbf1ae65173e56d53f3c24619d7eaabe
valerc123 <valentina.rua123@gmail.com>
2019-07-18 23:03:01 +0000

    malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - This function allocates memory using malloc
+ * @b : integer
+ * Return: pointer to the allocated memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(sizeof(int) * b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
