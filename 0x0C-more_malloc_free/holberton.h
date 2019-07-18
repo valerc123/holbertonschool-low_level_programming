aa0ae3764fc5cdc68ba66b55acfa220bdef8fa50
valerc123 <valentina.rua123@gmail.com>
2019-07-18 22:55:52 +0000

    firts commit

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,9 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int _putchar(char c);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+#endif
