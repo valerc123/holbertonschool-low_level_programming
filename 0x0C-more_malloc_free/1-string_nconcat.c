761a017fe6cb669605d0d149bf0e9a446106cb25
valerc123 <valentina.rua123@gmail.com>
2019-07-19 02:22:12 +0000

    string concat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - This function returns the length of a string
+ * @s: Variable that contains a character array to be counted
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int  n = 0;
+
+	while (*(s + n) != '\0')
+	{
+		n++;
+	}
+	return (n);
+}
+/**
+ * string_nconcat - This function concatenates two strings
+ * @s1: string one
+ * @s2: string two
+ * n: number of bytes given
+ * Return: pointer 
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len_s1;
+	char *concat;
+	int k, l;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len_s1 = _strlen(s1);
+	concat = malloc(sizeof(char) * (n + len_s1 + 1));
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{	
+		for (k = 0; s1[k] != '\0'; k++)
+		{
+			concat[k] = s1[k];
+		}		
+		for (l = 0; s2[l] != '\0'; l++)
+		{
+			concat[k + l] = s2[l];
+		}	
+	}
+	concat[k + l] = '\0';
+	return (concat);
+}
