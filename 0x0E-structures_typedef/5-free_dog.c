958d54b224c28ab6db40ff4fe0571bf8dfce27d3
valerc123 <valentina.rua123@gmail.com>
2019-07-22 23:17:58 +0000

    free doga

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
+ * free_dog - This function frees dogs
+ * @d: pointer dog
+ * Return: void
  */
 
 void free_dog(dog_t *d)
 {
-
+	if (d == NULL)
+	{
+		return;
+	}
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
