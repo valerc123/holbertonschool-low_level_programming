51e40ad8027e7095904225c71eaf5a281adcc7e9
valerc123 <valentina.rua123@gmail.com>
2019-07-24 23:45:09 +0000

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+/**
+ * get_op_func - selects correct function to perform operation
+ * @s: operator passed as argument
+ * Return: pointer to the function that corresponds to the operator given
+ * as a parameter
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (*ops[i].op == *s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "calc.h"
+#include <stdio.h>
+/**
+ * op_add - This function returns the sum of a and b
+ * @a: first digit
+ * @b: second digit
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
+/**
+ * op_sub - This function returns the difference of a and b
+ * @a: first digit
+ * @b: second digit
+ * Return: the substraction of a and b
+ */
+int op_sub(int a, int b)
+{
+	int result = a - b;
+
+	return (result);
+}
+/**
+ * op_mul - This function returns the product of a and b
+ * @a: first digit
+ * @b: second digit
+ * Return: the result of the multiplication
+ */
+int op_mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
+/**
+ * op_div - This function returns of the division of a by b
+ * @a: first digit
+ * @b: second digit
+ * Return: the result of the division
+ */
+int op_div(int a, int b)
+{
+	int result = a / b;
+
+	return (result);
+}
+/**
+ * op_mod - This function returns the remaider of the division of a by b
+ * @a: first digit
+ * @b: second digit
+ * Return: the substraction of a and b
+ */
+int op_mod(int a, int b)
+{
+	int result = a % b;
+
+	return (result);
+}
