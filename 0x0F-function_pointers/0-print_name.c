3625d259fd5d455146011aadaac8665d59774a90
valerc123 <valentina.rua123@gmail.com>
2019-07-24 20:30:14 +0000

    first commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - This function prints a name
+ * @name: name to print
+ * @f: pointer to funciton
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+	{
+		f(name);
+	}
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+int _putchar(char c);
+#endif
