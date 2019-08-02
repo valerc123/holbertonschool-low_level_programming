7734b58ebaf4f71146843e79b251f914e8fbe2aa
valerc123 <valentina.rua123@gmail.com>
2019-08-02 22:26:00 +0000

    advance

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * premain - execute before the main
+ */
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
