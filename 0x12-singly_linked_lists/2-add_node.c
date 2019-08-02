2c224f0bb44f7bd3d040a413d8dec92a62619f45
valerc123 <valentina.rua123@gmail.com>
2019-08-02 05:05:56 +0000

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+/**
+ * _strlen - Count the maximum number of a array
+ * @s: Input Array
+ *
+ * Return: counter
+ */
+
+int _strlen(const char *s)
+{
+	int count;
+
+	count = 0;
+	while (s[count])
+	{
+		count++;
+	}
+	return (count);
+}
+/**
+* add_node - This function adds a new node at the beginning of a list
+* @head: header of the list
+* @str: const name
+* Return: Address to new nodo
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	/*allocate new node*/
+	list_t *new_node =  malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	if (head == NULL)
+		return (NULL);
+
+
+	/*put in the data and copy*/
+	new_node->str = strdup(str);
+	/**/
+	new_node->len = _strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
