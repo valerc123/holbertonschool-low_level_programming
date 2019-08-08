81ef2d3440ff5d1a0f5b3afc32ee49a27ef8f2e6
valerc123 <valentina.rua123@gmail.com>
2019-08-08 19:47:31 +0000

    header

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,10 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+int _putchar(char c);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int clear_bit(unsigned long int *n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index);
+void print_binary(unsigned long int n);
+unsigned int binary_to_uint(const char *b);
+#endif
