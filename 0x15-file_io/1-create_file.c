f82e849ef23ea4e5fb095c3b04c6460a86d4cf9f
valerc123 <valentina.rua123@gmail.com>
2019-08-13 22:49:05 +0000

    create a file, add validations

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,23 +5,42 @@
  * @text_content: string to write in to the file
  * Return: 1 on success, -1 on failure
  */
+
+int _strlen(char *s)
+{
+	int  n = 0;
+
+	while (*(s + n) != '\0')
+	{
+		n++;
+	}
+	return (n);
+}
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-
+	int fd, len;
+	char *str;
+      
 	if (!filename)
-	return (-1);
+		return (-1);
 
-	fd = open(filename, O_CREAT | O_APPEND, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
-	{
-		write(1, "fails", 5);
+	{   
 		return (-1);
 	}
-	if (!text_content)
+	if (text_content == NULL)
 	{
-		creat(filename, 0600);
+		text_content = "";
 	}
+
+	len = _strlen(text_content);
+	str = malloc(sizeof(char) * len);
+	if (!str)
+		return(-1);
+
+	write(fd, text_content, len);
 	close(fd);
+	free(str);
 	return (1);
 }
