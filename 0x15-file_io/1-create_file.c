c21552bfeb5b52492ad92f60bc6841df0065b7dd
valerc123 <valentina.rua123@gmail.com>
2019-08-13 23:06:13 +0000

    create a file, add commets

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,9 @@
 #include "holberton.h"
 /**
- * create_file - This function creates a file
- * @filename: the name of the file to create
- * @text_content: string to write in to the file
- * Return: 1 on success, -1 on failure
+ * _strlen - This function returns the length of a string
+ * @s: Variable that contains a character array to be counted
+ * Return: Length of the string
  */
-
 int _strlen(char *s)
 {
 	int  n = 0;
@@ -16,17 +14,23 @@ int _strlen(char *s)
 	}
 	return (n);
 }
+/**
+ * create_file - This function creates a file
+ * @filename: the name of the file to create
+ * @text_content: string to write in to the file
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len;
 	char *str;
-      
+
 	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
-	{   
+	{
 		return (-1);
 	}
 	if (text_content == NULL)
@@ -37,7 +41,7 @@ int create_file(const char *filename, char *text_content)
 	len = _strlen(text_content);
 	str = malloc(sizeof(char) * len);
 	if (!str)
-		return(-1);
+		return (-1);
 
 	write(fd, text_content, len);
 	close(fd);
