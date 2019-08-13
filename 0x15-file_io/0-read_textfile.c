f9dce42eb48c17dc215f27a3c5401d98abdd1ae1
valerc123 <valentina.rua123@gmail.com>
2019-08-13 19:16:52 +0000

    first commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* read_textfile - reads a text file and prints it to the POSIX standard output.
+* @filename:
+* @letters:
+* Return:
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+
+	if (!filename)
+		return (0);
+
+}
