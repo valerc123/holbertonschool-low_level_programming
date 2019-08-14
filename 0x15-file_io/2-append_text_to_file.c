8fa8f2bda29d2cea44c2ce1d5577a4bc9f5acda8
valerc123 <valentina.rua123@gmail.com>
2019-08-14 00:20:30 +0000

    append text to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <errno.h>
+
+/**
+ * _strlen - This function returns the length of a string
+ * @s: Variable that contains a character array to be counted
+ * Return: Length of the string
+ */
+
+int _strlen(char *s)
+{
+	int  n = 0;
+
+	while (*(s + n) != '\0')
+	{
+		n++;
+	}
+	return (n);
+}
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: is the name of the file
+ * Return: 1 on success and -1 on failure
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+    int fd;
+
+    if (text_content == NULL)
+    {
+        text_content = "";
+    }
+    fd = open(filename, O_RDWR | O_APPEND);
+    if (fd == -1)
+    {
+        if (errno == ENOENT || errno == EROFS)
+        {
+            return (-1);
+        }
+    }
+    if (filename == NULL)
+    {
+        return (-1);
+    }
+    write(fd, text_content, _strlen(text_content));
+    close(fd);
+    return (1);
+}
