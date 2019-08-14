3afeb4321e2e1d2370f3396b602a56c3bec737e4
valerc123 <valentina.rua123@gmail.com>
2019-08-14 00:54:31 +0000

    append text, add comments

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,6 +20,7 @@ int _strlen(char *s)
 /**
  * append_text_to_file - appends text at the end of a file
  * @filename: is the name of the file
+ * @text_content: is the name of the file
  * Return: 1 on success and -1 on failure
 */
 int append_text_to_file(const char *filename, char *text_content)
