67c38a90fcda8fcd32160120d2683244f1d6b4d4
valerc123 <valentina.rua123@gmail.com>
2019-08-14 05:55:09 +0000

    copies content a other file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ * @argc: num of args
+ * @argv: args
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int size_to, size_from, file_to, file_from, close_file;
+	char buf[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	file_from = open(argv[1], O_RDWR);
+	if (file_from == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	size_from = read(file_from, buf, 1024);
+	while ((size_from = read(file_from, buf, 1024)) != 0)
+		/*checks for -1 or non 0 number */
+	{
+		if (size_from == -1)
+		{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		size_to = write(file_to, buf, size_from);
+
+		if (size_to == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+/* close file */
+	close_file = close(file_from);
+	if (close_file == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	close_file = close(file_to);
+	if (close_file == -1)
+	{	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+	}
+	return (0);
+}
