7c52e59b45532a62067ff5920ebb8e5a135e0c0b
valerc123 <valentina.rua123@gmail.com>
2019-08-14 18:39:38 +0000

    fix error read only

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,15 +15,14 @@ int main(int argc, char **argv)
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	file_from = open(argv[1], O_RDWR);
+	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	size_from = read(file_from, buf, 1024);
 	while ((size_from = read(file_from, buf, 1024)) != 0)
-		/*checks for -1 or non 0 number */
-	{
+	{	/*checks for -1 or non 0 number */
 		if (size_from == -1)
 		{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
