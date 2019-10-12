1da90619586873c19f59af9699adf68878eac816
valerc123 <valentina.rua123@gmail.com>
2019-10-12 01:49:36 +0000

    set hashtable

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,24 +1,54 @@
-
-
-/*
- * str_cmp() - Compare two strings to determine if they are identical. Ensures
- * that each char in the strings are the same, and that they are of the same
- * length.
- * @s1: The first string.
- * @s2: The second string.
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_table_set - set a value into a hash table
+ * @ht: Hash table
+ * @key: Key
+ * @value: Value
  *
- * Return: Zero if the strings are identical, one otherwise.
- */
-int str_cmp(char *s1, char *s2)
+ * Return: 1 - success or 0 - failure
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	int i;
-	int j = 0;
+	unsigned int index = 0;
+	hash_node_t *node;
 
-        for (i = 0; s1[i] != '\0'; ++i) {
-		if (s1[j] != s2[j] || s1[j + 1] != s2[j + 1]) {
-			return 1;
-		}
-		j++;
+	if (!ht || !key || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
+	{
+		free(ht->array[index]->value);
+		ht->array[index]->value = strdup(value);
+		return (1);
 	}
-	return 0;
+
+	node = create_node(key, value);
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);	
+}
+
+/**
+* create_node: creates a new node of hash_node_t type
+* @key: key
+* @value: value
+* Return: new node created
+*/
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	return (new_node);
 }
