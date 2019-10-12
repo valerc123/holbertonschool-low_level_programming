59c3d6cb8404ab9d624f1be3ae0617f96b315e81
valerc123 <valentina.rua123@gmail.com>
2019-10-12 02:38:05 +0000

    new variable for a value and key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,59 +9,26 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = 0;
-	hash_node_t *tmp;
-	char *dup_value, *dup_key;
+	unsigned int index_h = 0;
+	hash_node_t *temp;
 
-	if (!ht || !ht->array || ht->size == 0 || !key || strlen(key) == 0 || !value)
+	if (!ht || !key || !value)
 		return (0);
 
-	dup_key = strdup(key);
-	dup_value = strdup(value);
-
-	index = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->array[index];
-
-	while (tmp != NULL)
+	index_h = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index_h] != NULL && strcmp(ht->array[index_h]->key, key) == 0)
 	{
-		if (strcmp(tmp->key, dup_key) == 0)
-		{
-			tmp->value = dup_value;
-			return (1);
-		}
-		tmp = tmp->next;
+		free(ht->array[index_h]->value);
+		ht->array[index_h]->value = strdup(value);
+		return (1);
 	}
-
-	tmp = create_node(key, value);
-	if (!tmp)
+	temp = malloc(sizeof(hash_node_t));
+	if (!temp)
 		return (0);
-	tmp->next = ht->array[index];
-	ht->array[index] = tmp;
+
+	temp->key = strdup(key);
+	temp->value = strdup(value);
+	temp->next = ht->array[index_h];
+	ht->array[index_h] = temp;
 	return (1);
-}
-
-/**
-* create_node - Creates a new node of hash_node_t type
-* @key: key for the node
-* @value: value for the node
-* Return: new node created, or NULL on failure
-*/
-hash_node_t *create_node(const char *key, const char *value)
-{
-	hash_node_t *new_node;
-
-	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
-		return (NULL);
-
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = NULL;
-
-	if (!new_node->key || !new_node->value)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	return (new_node);
 }
