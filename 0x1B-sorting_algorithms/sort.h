47dd516c7b6a6cfed39b249e6286c1406feeab21
valerc123 <valentina.rua123@gmail.com>
2019-10-28 20:50:50 +0000

    header

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -1,6 +1,9 @@
 #ifndef _SORT_H_
 #define _SORT_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -17,4 +20,5 @@ typedef struct listint_s
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
 #endif
