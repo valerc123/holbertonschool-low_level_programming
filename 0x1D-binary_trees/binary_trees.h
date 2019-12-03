4de694bde97a24aff2c5c7514f73caa3dc563514
valerc123 <valentina.rua123@gmail.com>
2019-12-03 18:40:37 +0000

    header

--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -1,6 +1,10 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -42,4 +46,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+
+/* Print Binary trees */
+void binary_tree_print(const binary_tree_t *);
 #endif
