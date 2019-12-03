a305c3edeec10d5b25e9f386b31afc3d835a6662
valerc123 <valentina.rua123@gmail.com>
2019-12-03 19:06:26 +0000

    Betty

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-/*
+/**
  * binary_tree_node - This function creates a binary tree node
  * @parent: pointer to the parent node
  * @value: value to put in the new node
@@ -9,14 +9,15 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-        if (!new_node)
-                return (NULL);
+	if (!new_node)
+		return (NULL);
 
-        new_node->n = value;
-        new_node->parent = parent;
-        new_node->left = NULL;
-        new_node->right = NULL;
-        return (new_node);
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
 }
--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -1,33 +1,34 @@
 #include "binary_trees.h"
 
-/*
- * binary_tree_insert_right - This function inserts a node as the right-child of another node
- * @parent: pointer to the node to insert the right-child in
- * @value: value to store in the new node
- *
- * Return: pointer of new node or NULL on failure or if parent is NULL
- */
+/**
+* binary_tree_insert_right - This function inserts a node as the
+* right-child of another node
+* @parent: pointer to the node to insert the right-child in
+* @value: value to store in the new node
+*
+* Return: pointer of new node or NULL on failure or if parent is NULL
+*/
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-        binary_tree_t *tmp;
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *tmp;
 
-        if (!new_node || !parent)
-                return (NULL);
+	if (!new_node || !parent)
+		return (NULL);
 
-        new_node->parent = parent;
-        if (new_node->parent->right)
-        {
-                tmp = new_node->parent->right;
-                new_node->parent->right = new_node;
-                new_node->parent->right->right = tmp;
-        }
-        else
-        {
-                new_node->parent->right = new_node;
-        }
-        new_node->n = value;
+	new_node->parent = parent;
+	new_node->n = value;
+	if (new_node->parent->right)
+	{
+		tmp = new_node->parent->right;
+		new_node->parent->right = new_node;
+		new_node->parent->right->right = tmp;
+	}
+	else
+	{
+		new_node->parent->right = new_node;
+	}
 
-        return (new_node);
+	return (new_node);
 }
