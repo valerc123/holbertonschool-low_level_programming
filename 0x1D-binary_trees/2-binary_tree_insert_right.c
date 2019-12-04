c9a21079cb984240a51e99d11dbe1e39a7cda04c
valerc123 <valentina.rua123@gmail.com>
2019-12-04 18:01:24 +0000

    call function create node

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -11,16 +11,15 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node;
 	binary_tree_t *tmp = NULL;
 
-	if (!new_node || !parent)
+	if (!parent)
 		return (NULL);
 
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
+	new_node = binary_tree_node(parent, value);
+	if (!new_node)
+		return (NULL);
 
 	if (parent->right)
 	{
