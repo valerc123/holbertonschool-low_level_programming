d7876fdc39da43010823aafefabfeff249de38bf
valerc123 <valentina.rua123@gmail.com>
2019-12-04 18:54:44 +0000

    chevk

--- 0x1D-binary_trees/4-binary_tree_is_leaf.c
+++ 0x1D-binary_trees/4-binary_tree_is_leaf.c
@@ -8,13 +8,12 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+	if (!node)
+		return (0);
+
 	if (!node->left && !node->right)
 		return (1);
 	else
 		return (0);
-
-	if (!node)
-		return (0);
-
 	return (0);
 }
