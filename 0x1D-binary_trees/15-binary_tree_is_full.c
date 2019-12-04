822d3c2c6d45794a8b80048fe82589abc4d1018d
valerc123 <valentina.rua123@gmail.com>
2019-12-04 23:22:54 +0000

    Check if tree is full

--- 0x1D-binary_trees/15-binary_tree_is_full.c
+++ 0x1D-binary_trees/15-binary_tree_is_full.c
@@ -7,30 +7,29 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    int left, right;
-    if (!tree)
-        return (0);
-    if (tree->left)
-    {
-        left = binary_tree_is_full(tree->left);
-    }
-    if (tree->right)
-    {
-        right = binary_tree_is_full(tree->right);
-    }
-    if (tree->right && tree->left)
-    {
-        if (left != right)
-        {
-            return (0);
-        }
-        else
-        {
-            return (1);
-        }
-    }
-    else
-    {
-        return (0);
-    }
+	int left = 0, right = 0;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+	{
+		return (1);
+	}
+
+	if (tree->left)
+	{
+		left = binary_tree_is_full(tree->left);
+	}
+	if (tree->right)
+	{
+		right = binary_tree_is_full(tree->right);
+	}
+	if (right == 1 && left == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
