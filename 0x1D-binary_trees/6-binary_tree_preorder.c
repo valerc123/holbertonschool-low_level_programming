eeec6cebbdb243696ce6b01b72240e582fa14d3e
valerc123 <valentina.rua123@gmail.com>
2019-12-04 19:06:10 +0000

    Pre- order

--- 0x1D-binary_trees/6-binary_tree_preorder.c
+++ 0x1D-binary_trees/6-binary_tree_preorder.c
@@ -11,11 +11,13 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
+	if (func == NULL || tree == NULL)
+		return;
+
 	func(tree->n);
-	if ((tree->left && tree->left->left) || tree->right)
+	if (tree->left)
 		binary_tree_preorder(tree->left, func);
 
-	if ((tree->right && tree->right->right) || tree->left)
+	if (tree->right)
 		binary_tree_preorder(tree->right, func);
-	
 }
